5c6da149c270a548a1a3fab52f5c454f9c30c772
marcelo vinicius <viniciusmarcello12@gmail.com>
2021-09-09 20:14:13 -0300

    corrigindo errros , fazendo sorteio e adicionando atributos pelo JS

--- js/funcoes.js
+++ js/funcoes.js
@@ -17,17 +17,34 @@ const batman = {
 }
 };
 const cartas = [ naruto , batman]; /*declaração de quais sao as cartas*/
+let cartaJogador, cartaMaquina;
 function SortearCarta(){          
     const numeroDeCartas = 2; 
     let numeroCartaJogador = parseInt(Math.random()*numeroDeCartas); /*codigo para sortear as cartas*/
     let numeroCartaMaquina = parseInt(Math.random()*numeroDeCartas);
     while(numeroCartaJogador == numeroCartaMaquina){ /*while faz que nao haja repetição de carta pq enquanto as duus forem iguais ele repete o sorteio*/
-        numeroCartaJogador == parseInt(Math.random()*numeroDeCartas);
+        numeroCartaJogador = parseInt(Math.random()*numeroDeCartas);
     }
-const cartaJogador = cartas[numeroCartaJogador]; 
-const cartaMaquina = cartas[numeroCartaMaquina]; /*declara que carta maquina é igual ao seu numero de carta sorteado*/
-console.log(cartaJogador , cartaMaquina);
+cartaJogador = cartas[numeroCartaJogador]; 
+cartaMaquina = cartas[numeroCartaMaquina]; /*declara que carta maquina é igual ao seu numero de carta sorteado*/
+ExibirCartaJogador();
 }
+function ExibirCartaJogador(){
+    let divCartaJogador = document.querySelector("#carta-jogador");
+    divCartaJogador.style.backgroundImage = `url(${cartaJogador.Imagem})`;
+    document.querySelector(".nome-personagem-jogador").innerHTML = cartaJogador.nome;
+    //listar atributos//
+    let listaDeAtributosDaCarta = document.querySelector(".atributos-jogador");
+    listaDeAtributosDaCarta.innerHTML = PegarAtributos(cartaJogador.atributos);
+}
+function PegarAtributos(atributosDaCarta){
+    let listaDeAtributos = "";
+    for(let atributo in atributosDaCarta){
+        listaDeAtributos += `<li>${atributo} : ${atributosDaCarta[atributo]}</li>`;
+    }
+    return listaDeAtributos;
+}
+
 const btnSortear = document.querySelector("#btnSortear"); /*faz com que o botao faça o sorteio das cartas */
 btnSortear.onclick = () => {
     SortearCarta();
